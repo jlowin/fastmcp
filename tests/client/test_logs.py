8dca04513b6cbd7d8b21ca3781af370522534228
Colin Jermain <colin@realm.security>
2025-08-01 13:43:34 -0400

    Adding extra kwarg to client-side logging to support Python standard library API for structured logs

--- tests/client/test_logs.py
+++ tests/client/test_logs.py
@@ -1,16 +1,26 @@
+import logging
+
 import pytest
 from mcp import LoggingLevel
 
 from fastmcp import Client, Context, FastMCP
 from fastmcp.client.logging import LogMessage
 
+LOGGING_LEVEL_MAP = logging.getLevelNamesMapping()  # pyright: ignore [reportAttributeAccessIssue]
+
 
 class LogHandler:
     def __init__(self):
         self.logs: list[LogMessage] = []
+        self.logger = logging.getLogger(__name__)
 
     async def handle_log(self, message: LogMessage) -> None:
         self.logs.append(message)
+
+        level = LOGGING_LEVEL_MAP[message.level.upper()]
+        msg = message.data.get("msg")
+        extra = message.data.get("extra")
+        self.logger.log(level, msg, extra=extra)
 
 
 @pytest.fixture
@@ -34,27 +44,42 @@ def fastmcp_server():
 
 
 class TestClientLogs:
-    async def test_log(self, fastmcp_server: FastMCP):
+    async def test_log(self, fastmcp_server: FastMCP, caplog):
+        caplog.set_level(logging.INFO, logger=__name__)
+
         log_handler = LogHandler()
         async with Client(fastmcp_server, log_handler=log_handler.handle_log) as client:
             await client.call_tool("log", {})
 
         assert len(log_handler.logs) == 1
-        assert log_handler.logs[0].data == "hello?"
+        assert log_handler.logs[0].data["msg"] == "hello?"
         assert log_handler.logs[0].level == "info"
 
-    async def test_echo_log(self, fastmcp_server: FastMCP):
+        assert len(caplog.records) == 1
+        assert caplog.records[0].msg == "hello?"
+        assert caplog.records[0].levelname == "INFO"
+
+    async def test_echo_log(self, fastmcp_server: FastMCP, caplog):
+        caplog.set_level(logging.INFO, logger=__name__)
+
         log_handler = LogHandler()
         async with Client(fastmcp_server, log_handler=log_handler.handle_log) as client:
             await client.call_tool("echo_log", {"message": "this is a log"})
 
             assert len(log_handler.logs) == 1
+            assert len(caplog.records) == 1
             await client.call_tool(
                 "echo_log", {"message": "this is a warning log", "level": "warning"}
             )
             assert len(log_handler.logs) == 2
+            assert len(caplog.records) == 2
 
-        assert log_handler.logs[0].data == "this is a log"
+        assert log_handler.logs[0].data["msg"] == "this is a log"
         assert log_handler.logs[0].level == "info"
-        assert log_handler.logs[1].data == "this is a warning log"
+        assert log_handler.logs[1].data["msg"] == "this is a warning log"
         assert log_handler.logs[1].level == "warning"
+
+        assert caplog.records[0].msg == "this is a log"
+        assert caplog.records[0].levelname == "INFO"
+        assert caplog.records[1].msg == "this is a warning log"
+        assert caplog.records[1].levelname == "WARNING"
