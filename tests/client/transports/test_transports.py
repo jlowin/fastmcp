d59f8e555db85d5bce7584136fa817b4e18d7883
Giovanna <giovanna.zanardini@nextroll.com>
2025-10-31 17:14:47 -0300

    Fix test

--- tests/client/transports/test_transports.py
+++ tests/client/transports/test_transports.py
@@ -1,14 +1,21 @@
 from ssl import VerifyMode
 
-from fastmcp.client.transports import StreamableHttpTransport
 import httpx
+
+from fastmcp.client.transports import StreamableHttpTransport
+
 
 async def test_oauth_uses_same_client_as_transport():
     transport = StreamableHttpTransport(
         "https://some.fake.url/",
-        httpx_client_factory=lambda *args, **kwargs: httpx.AsyncClient(verify=False, *args, **kwargs),
+        httpx_client_factory=lambda *args, **kwargs: httpx.AsyncClient(
+            verify=False, *args, **kwargs
+        ),
         auth="oauth",
     )
 
     async with transport.auth.httpx_client_factory() as httpx_client:
-        assert httpx_client._transport._pool._ssl_context.verify_mode == VerifyMode.CERT_NONE
+        assert (
+            httpx_client._transport._pool._ssl_context.verify_mode
+            == VerifyMode.CERT_NONE
+        )
